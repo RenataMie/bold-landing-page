f6bd264d4b6a5777317a1eee12c7499ce296bf4d
Renata Mie <renata_mie@hotmail.com>
2021-04-13 21:26:53 -0300

    phone version

--- app.js
+++ app.js
@@ -14,19 +14,63 @@ function fetchData () {
 				<div class="gridProduct">
 					<img src="https:${prod.image}" alt="${prod.name}">
 					<h4> ${prod.name}</h4>
-					<p> ${prod.description}</p>
-					<p>De: R$23,99</p>
-					<h3>Por: R$19,99</h3>
-					<p>ou 2x de R$9,99</p>
+					<p class="descricao"> ${prod.description}</p>
+					<p>De: R$ ${prod.oldPrice}</p>
+					<h3>Por: R$ ${prod.price}</h3>
+					<p>ou ${prod.installments.count}x de R$ ${prod.installments.value}</p>
 					<button>Comprar</button>
 				</div>
 				`;
 			})
 			.join("")
 		document.querySelector("#products").insertAdjacentHTML("afterbegin",html);
+
 	})
-	
 	
 }
 
 fetchData();
+
+
+function fetchData2 () {
+	fetch("https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=2")
+	.then(response => {
+		if (!response.ok) {
+			throw Error("Error!");
+		}
+		return response.json();
+	})
+	.then(data => {
+		console.log(data.products);
+		const html = data.products
+			.map(prod => {
+				return `
+				<div class="gridProduct">
+					<img src="https:${prod.image}" alt="${prod.name}">
+					<h4> ${prod.name}</h4>
+					<p> ${prod.description}</p>
+					<p>De: R$ ${prod.oldPrice}</p>
+					<h3>Por: R$ ${prod.price}</h3>
+					<p>ou ${prod.installments.count}x de R$ ${prod.installments.value}</p>
+					<button>Comprar</button>
+				</div>
+				`;
+			})
+			.join("")
+		document.querySelector("#products").insertAdjacentHTML("beforeend",html);
+	})
+	
+}
+
+document.getElementById("Btn").addEventListener("click", fetchData2); 
+
+document.getElementById("Email").addEventListener("input", function (event) {
+  if (document.getElementById("Email").validity.typeMismatch) {
+    document.getElementById("Email").setCustomValidity("Email inválido!");
+    document.getElementById("Email").style.borderColor = "red";
+
+
+  } else {
+   document.getElementById("Email").setCustomValidity("");
+  }
+});
